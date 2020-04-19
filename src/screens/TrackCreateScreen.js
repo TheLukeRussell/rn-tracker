62c55b93fb14693cc8de26d98db3b11f5800ebfc
Luke Russell <thelukerussel21@gmail.com>
2020-04-19 17:07:56 -0400

    added map to app

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
+import { requestPermissionsAsync } from 'expo-location';
+import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+  const startWatching = async () => {
+    try {
+      await requestPermissionsAsync();
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
-      <Text>Track Create</Text>
+      <Text h2>Create A Track</Text>
+      <Map />
+      {err ? <Text>Please enable Location Services</Text> : null}
     </SafeAreaView>
   );
 };
